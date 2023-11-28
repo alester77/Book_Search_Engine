fd458c636e766d160e3b56a765f6a8818c3597bf
alester77 <113203488+alester77@users.noreply.github.com>
2023-11-27 18:26:48 -0700

    psuedo coding resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,18 @@
+const { AuthenicationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+// Query
+
+// Mutation
+
+  //login
+  //addUser
+  //saveBook
+  //removeBook
+
+}
+
+module.exports = resolvers;
