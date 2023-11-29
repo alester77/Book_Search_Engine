5ac498dee7bb4d7a0779ccdf19b5c0d65a413cda
alester77 <113203488+alester77@users.noreply.github.com>
2023-11-29 11:15:50 -0700

    added query to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,19 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
 
 // Query
+Query: {
+  me: async(parent, args, context) => {
+    if (context.user) {
+      const userData = await User
+      .findOne({ _id: context.user._id })
+      .select('-__v -password')
+      .populate('books');
+
+      return userData;
+    };
+    throw new AuthenicationError('Not logged in');
+    },
+},
 
 // Mutation
 
