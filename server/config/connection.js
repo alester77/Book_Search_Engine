ad1a3b399a6ae7907c02d234f6f87155c2b49959
alester77 <113203488+alester77@users.noreply.github.com>
2023-12-07 15:52:19 -0700

    please work mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://alester0284:v6BU!nyZSqtj64tMfDcr9@booksearch.vd0xbcj.mongodb.net/',{
+}
+);
 
 module.exports = mongoose.connection;
